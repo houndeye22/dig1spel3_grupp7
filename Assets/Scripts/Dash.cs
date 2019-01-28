4ce15d3d7f02479d210612e1d558913600ef7aa1
Viktor.Carlsson <viktor.carlsson@elev.ga.lbs.se>
2019-01-28 10:17:15 +0100

    Dash (not working)

---
+++ Assets/Scripts/Dash.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dash : MonoBehaviour
+{
+
+    public float dashSpeed;
+    private float dashTime;
+    public float dashStartTime;
+    private int direction;
+
+    Rigidbody2D rbodyDash;
+
+    
+    void Start()
+    {
+        rbodyDash = GetComponent<Rigidbody2D>();
+        dashTime = dashStartTime;
+    }
+
+    private void Update()
+    {
+    if (direction == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                direction = 1;
+            }
+            else if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                direction = 2;
+            }
+            else if (Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                direction = 3;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow))
+            {
+                direction = 4;
+            }
+
+        }
+            else
+            {
+                if (dashTime <= 0)
+                {
+                    direction = 0;
+                    dashTime = dashStartTime;
+                    rbodyDash.velocity = Vector2.zero;
+                }
+                else
+                {
+                    dashTime -= Time.deltaTime;
+
+                    if (direction == 1)
+                    {
+                        rbodyDash.velocity = Vector2.left* dashSpeed;
+                    }
+                else if (direction == 2)
+                    {
+                        rbodyDash.velocity = Vector2.right* dashSpeed;
+                    }
+                }
+            }
+    }
+}
--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -7,22 +7,62 @@ public class Movement : MonoBehaviour
     Rigidbody2D rbod1;
     public int moveSpeed;
     public float jumpThrust;
-    
+    public float dashSpeed;
+    public float dashTime;
+    public float dashStart;
+    private int direction;
+
 
     // Start is called before the first frame update
     void Start()
     {
+        dashSpeed = 4;
         rbod1 = GetComponent<Rigidbody2D>();
-
-
+        dashTime = dashStart;
     }
 
     // Update is called once per frame
+    private void Update()
+    {
+        if (direction == 0)
+        {
+            if (Input.GetKeyDown(KeyCode.RightArrow))
+            {
+                direction = 2;
+            }
+            else if (Input.GetKeyDown(KeyCode.LeftArrow))
+            {
+                direction = 1;
+            }
+            else
+            {
+                if (dashTime <= 0)
+                {
+                    direction = 0;
+                    dashTime = dashStart;
+                    rbod1.velocity = Vector2.zero;
+                }
+                else
+                {
+                    dashTime -= Time.deltaTime;
+
+                    if (direction == 1)
+                    {
+                        rbod1.velocity = Vector2.left * dashSpeed;
+                    } else
+                    {
+                        rbod1.velocity = Vector2.right * dashSpeed;
+                    }
+                }
+            }
+        }
+    }
+
     void FixedUpdate()
     {
         rbod1.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rbod1.velocity.y);
 
-        if (Input.GetAxisRaw("Vertical") > 0)
+        if (Input.GetAxisRaw("Jump") > 0)
         {
             if (isGrounded == true)
             {
@@ -30,6 +70,7 @@ public class Movement : MonoBehaviour
             }
         }
     }
+
 
 
 
