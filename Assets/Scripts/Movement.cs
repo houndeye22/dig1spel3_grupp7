20631b4e66f9f16891c5dead3b7e5693b859c9fd
houndeye22 <erik.engstrom@elev.ga.lbs.se>
2019-01-28 08:26:26 +0100

    movement

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    Rigidbody2D rbod1;
+    public int moveSpeed;
+    public float jumpThrust;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbod1 = GetComponent<Rigidbody2D>();
+
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        rbod1.velocity = new Vector2(Input.GetAxis("Horizontal") * moveSpeed, rbod1.velocity.y);
+
+        if (Input.GetAxisRaw("Vertical") > 0)
+        {
+            if (isGrounded == true)
+            {
+                rbod1.velocity = new Vector2(rbod1.velocity.x, jumpThrust);
+            }
+        }
+    }
+
+
+
+    public bool isGrounded;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        isGrounded = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        isGrounded = false;
+    }
+}
