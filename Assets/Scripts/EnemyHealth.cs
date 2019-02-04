4a0377708b43f9e234940be0d1d37245f745ec27
Emrik Lind <emrik.lind@elev.ga.lbs.se>
2019-02-04 10:54:49 +0100

    Enemy heath and attack dmg done

---
+++ Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHealth;
+    public int currentHealth;
+
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Sword")
+        {
+            currentHealth--;
+        }
+    }
+     void Update()
+    {
+        if (currentHealth == 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
