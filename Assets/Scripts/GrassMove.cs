7383691b87203c26c1ac10b9e2d14ac45a7b23ac
Viktor.Carlsson <viktor.carlsson@elev.ga.lbs.se>
2019-03-11 08:22:23 +0100

    AHHHHH

---
+++ Assets/Scripts/GrassMove.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrassMove : MonoBehaviour
+{
+    private Animator move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        move = GetComponent<Animator>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("player"))
+        {
+            move.SetTrigger("move"); 
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/IfFallOut.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IfFallOut : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        HPScript.healthRemaining = 0;
+        
+    }
+}
