3b17ef85c6b21b391ed71c2282babe5bdbc17364
Viktor.Carlsson <viktor.carlsson@elev.ga.lbs.se>
2019-03-18 10:46:49 +0100

    OUR HOUSE

--- Assets/Scripts/Pause.cs
+++ Assets/Scripts/Pause.cs
@@ -31,7 +31,7 @@ public class Pause : MonoBehaviour
             Deactivate();
         }
 
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKeyDown(KeyCode.Escape))
         {
             isPaused = !isPaused;
         }
