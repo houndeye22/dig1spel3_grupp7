917ab181e627bfba8c82e96cb62fe02a1acb8267
william <william.lyckner@elev.ga.lbs.se>
2019-03-19 09:46:03 +0100

    jag har gjort en fiende

---
+++ Assets/hurt.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; //lägger till unityengines för scene managements
+public class hurt : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {    //ifall ett objekt med tagen player koliderar med detta objektet aktiveras en ny scen
+        if (collision.collider.tag == "Player")
+        {
+            Scene active = SceneManager.GetActiveScene();
+            SceneManager.LoadScene(active.name);
+        }
+    }
+
+}
